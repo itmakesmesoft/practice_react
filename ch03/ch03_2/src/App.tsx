1af51d6cfdcfedc5604507f8d88acc11baef146b
itmakesmesoft <dmsgur7112@gmail.com>
2023-02-24 17:52:33 +0900

    공부 기록

---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,14 @@
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextsTest'
+
+function App() {
+  return (
+    <div>
+      <TextsTest />
+      <Color />
+      <Tailwindcss />
+    </div>
+  )
+}
+export default App
